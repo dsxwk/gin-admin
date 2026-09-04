bf7cda69e6cee9eb1ad721c8adbbcef5654cde77
dsx <25076778@qq.com>
2026-09-04 14:04:54 +0800

    优化公共响应支持原生err和自定义err

--- common/response/errcode.go
+++ common/response/errcode.go
@@ -1,8 +1,11 @@
 package response
 
 import (
+	"errors"
 	"gin/common/errcode"
+	"gin/pkg/serviceprovider/lang"
 	"gin/pkg/serviceprovider/logger"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -30,43 +33,92 @@ func (r Response) json(c *gin.Context, httpCode int) {
 }
 
 // Success 返回成功响应,可传ErrorCode
-func (r Response) Success(c *gin.Context, e errcode.ErrorCode) {
-	r.Code = e.Code
-	if e.Msg == "" {
-		r.Msg = errcode.Success().Msg
-	} else {
-		r.Msg = e.Msg
-	}
-	if e.Data == nil {
-		r.Data = []string{}
-	} else {
-		r.Data = e.Data
-	}
-	if e.HttpCode == 0 {
-		e.HttpCode = 200
+func (r Response) Success(c *gin.Context, e error) {
+	var (
+		ec  errcode.ErrorCode
+		ctx = c.Request.Context()
+	)
+	if e != nil && errors.As(e, &ec) {
+		r.Code = ec.Code
+		if ec.Msg != "" {
+			r.Msg = ec.Msg
+			r.Msg = lang.Trans(ctx, ec.Msg, nil)
+		} else {
+			r.Msg = errcode.Success().Msg
+			r.Msg = lang.Trans(ctx, r.Msg, nil)
+		}
+		if ec.Data == nil {
+			r.Data = []any{}
+		} else {
+			r.Data = ec.Data
+		}
+		if ec.HttpCode == 0 {
+			ec.HttpCode = 200
+		}
+
+		r.json(c, ec.HttpCode)
+		return
 	}
 
-	r.json(c, e.HttpCode)
+	// 普通error
+	r.Code = 0
+	if e != nil {
+		r.Msg = e.Error()
+		r.Msg = lang.Trans(ctx, r.Msg, nil)
+	} else {
+		r.Msg = errcode.Success().Msg
+		r.Msg = lang.Trans(ctx, r.Msg, nil)
+	}
+	r.Data = []any{}
+	r.json(c, http.StatusOK)
 }
 
 // Error 返回失败响应,可传ErrorCode
-func (r Response) Error(c *gin.Context, e errcode.ErrorCode) {
-	r.Code = e.Code
-	if e.Msg != "" {
-		r.Msg = e.Msg
-		log.WithDebugger(c.Request.Context()).Error(e.Msg)
-	} else {
-		r.Msg = errcode.SystemError().Msg
-		log.WithDebugger(c.Request.Context()).Error(errcode.SystemError().Msg)
-	}
-	if e.Data == nil {
-		r.Data = []string{}
-	} else {
-		r.Data = e.Data
-	}
-	if e.HttpCode == 0 {
-		e.HttpCode = 500
+func (r Response) Error(c *gin.Context, e error) {
+	var (
+		ec  errcode.ErrorCode
+		ctx = c.Request.Context()
+	)
+	// ErrorCode类型
+	if e != nil && errors.As(e, &ec) {
+		r.Code = ec.Code
+		if ec.Msg != "" {
+			r.Msg = ec.Msg
+			r.Msg = lang.Trans(ctx, r.Msg, nil)
+			log.WithDebugger(ctx).Error(r.Msg)
+		} else {
+			r.Msg = errcode.Success().Msg
+			r.Msg = lang.Trans(ctx, r.Msg, nil)
+			log.WithDebugger(ctx).Error(r.Msg)
+		}
+		if ec.Data == nil {
+			r.Data = []any{}
+		} else {
+			r.Data = ec.Data
+		}
+		if ec.HttpCode == 0 {
+			ec.HttpCode = 500
+		}
+
+		r.json(c, ec.HttpCode)
+		return
 	}
 
-	r.json(c, e.HttpCode)
+	// 普通error
+	r.Code = errcode.SystemError().Code
+	if e != nil {
+		r.Msg = e.Error()
+		r.Msg = lang.Trans(ctx, r.Msg, nil)
+		if log != nil {
+			log.WithDebugger(ctx).Error(r.Msg)
+		}
+	} else {
+		r.Msg = errcode.SystemError().Msg
+		r.Msg = lang.Trans(ctx, r.Msg, nil)
+		if log != nil {
+			log.WithDebugger(ctx).Error(r.Msg)
+		}
+	}
+	r.Data = []any{}
+	r.json(c, http.StatusInternalServerError)
 }
